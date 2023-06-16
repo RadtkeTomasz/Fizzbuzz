5312c278598cabfb3c1f83da762f0094ad32620b
TomaszR <tomeas23@email.com>
2023-06-16 11:57:03 +0200

    Fibonacci Addition

---
+++ Fizzbuzz.Tests/FibonacciTests.cs
@@ -0,0 +1,58 @@
+﻿using Oponeo.UnitTests.Implementation;
+
+namespace Fizzbuzz.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        [DataRow(-5)]
+        public void Should_Throw_ArgumentOutOfRangeException_When_Index_Below_0(int index)
+        {
+            try
+            {
+                // Arrange
+                var fibonacci = new Fibonacci();
+
+                // Act
+                fibonacci.CalculateFibonacciIndexValue(index);
+
+                // Assert
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException) 
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(-5)]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_Throw_ArgumentOutOfRangeException_When_Index_Below_0_Attribute(int index)
+        {
+                // Arrange
+                var fibonacci = new Fibonacci();
+
+                // Act
+                fibonacci.CalculateFibonacciIndexValue(index);
+
+                // Assert
+                Assert.Fail();
+        }
+
+        [TestMethod]
+        [DataRow(5)]
+        public void Should_Return_5_When_Index_Is_5(int index)
+        {
+            // Arrange
+            var fibonacci = new Fibonacci();
+
+            // Act
+            var result = fibonacci.CalculateFibonacciIndexValue(index);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ Fizzbuzz/Fibonacci.cs
@@ -0,0 +1,14 @@
+﻿namespace Oponeo.UnitTests.Implementation
+{
+    public class Fibonacci
+    {
+        public int CalculateFibonacciIndexValue(int index)
+        {
+            if ( index < 0 )
+                throw new ArgumentOutOfRangeException();
+            if ( index == 0 || index == 1 )
+                return index;
+            return CalculateFibonacciIndexValue(index - 1) + CalculateFibonacciIndexValue(index - 2);
+        }
+    }
+}
